076a9a6db670e1180c7cf91b368f6794e7f85fb5
srj <sreejithcs273@gmail.com>
2025-05-31 14:49:25 +0000

    ami

--- terraform-file.tf
+++ terraform-file.tf
@@ -13,7 +13,7 @@ terraform {
 
 resource "aws_instance" "node-server" {
 
- ami = "ami-084568db4383264d4"
+ ami = "ami-04f167a56786e4b09"
  instance_type = "t2.micro"
  key_name = "demokey"
  user_data = <<-EOF
