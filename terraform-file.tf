2c5e9f6b3d0b7348b1db0717d63a54653f2a1ff5
srj <sreejithcs273@gmail.com>
2025-05-31 16:47:37 +0000

    Revert "ami"
    This reverts commit d308d235608c8e9807c173f9460da9b134c4473a.

--- terraform-file.tf
+++ terraform-file.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "node-server" {
 
  ami = "ami-04f167a56786e4b09"
  instance_type = "t2.micro"
- key_name = "demokeynew"
+ key_name = "demokey"
  user_data = <<-EOF
              #!/bin/bash
              apt update
