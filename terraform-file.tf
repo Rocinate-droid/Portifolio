894e6bf7a7929ae56893b5e16a2a9cddda7e3d37
srj <sreejithcs273@gmail.com>
2025-06-01 06:07:55 +0000

    key changed

--- terraform-file.tf
+++ terraform-file.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "node-server" {
 
  ami = "ami-06c8f2ec674c67112"
  instance_type = "t2.micro"
- key_name = "demokey"
+ key_name = "demokeynew"
  user_data = <<-EOF
              #!/bin/bash
              apt update
